deed28fce33c22a518d6d07830514c0fb44679b8
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-03 22:33:16 +0900

    default wifi module set
    Change-Id: Ibd3876745cade7655d3dbc18218ed3313b92b124

--- macloader/macloader.cpp
+++ macloader/macloader.cpp
@@ -71,9 +71,12 @@ int main() {
     }
 
     /* semcosh */
-    if (strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0) {
+    else if (strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0) {
         type = SEMCOSH;
     }
+
+    /* default set to MURATA */
+    else { type = MURATA; }
 
     if (type != NONE) {
         /* open cid file */
